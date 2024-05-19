3963d741ac44ff1b406412a269fb307c261384dc
kurotu <kurotu@gmail.com>
2024-05-19 15:54:20 +0900

    Fix invalid repository json by any-json

--- tasks/download-repos.sh
+++ tasks/download-repos.sh
@@ -3,12 +3,30 @@ set -eu
 ROOT="$(cd "$(dirname "$0")" && pwd)/.."
 DIR="$ROOT/vpm/repos"
 
+function fixJson() {
+  local TEMP
+  TEMP="$(mktemp)"
+  cat - > "$TEMP"
+  npx any-json --input-format=hjson "$TEMP"
+  rm "$TEMP"
+}
+
 function download() {
   local URL=$1
   local CONTENT
+
   CONTENT=$(curl -s -L -H "User-Agent: VPM Catalog" "$URL")
   # remove bom from content
   CONTENT=$(echo "$CONTENT" | sed '1s/^\xEF\xBB\xBF//')
+  # remove trailing commas
+  set +e
+  if ! echo "$CONTENT" | jq . > /dev/null; then
+    set -e
+    echo "Fix JSON by any-json"
+    CONTENT=$(fixJson <<< "$CONTENT")
+  fi
+  set -e
+
   local ID
   ID=$(echo "$CONTENT" | jq -r '.id')
   echo "$CONTENT" > "$DIR/$ID.json"
@@ -16,6 +34,9 @@ function download() {
 
 mkdir -p "$DIR"
 while read -r URL; do
+  if [ "${URL:0:1}" == "#" ]; then
+    continue
+  fi
   echo "Downloading $URL"
   download "$URL"
 done < "$ROOT/repositories.txt"
