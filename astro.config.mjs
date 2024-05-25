574f5f095b92fd769d2a7422958b1a4ec867729f
kurotu <kurotu@gmail.com>
2024-05-25 12:23:02 +0900

    Tweak ogp properties

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import remarkToc from 'remark-toc';
 import tailwind from "@astrojs/tailwind";
 
+let site = process.env['VERCEL'] ? `https://${process.env['VERCEL_PROJECT_PRODUCTION_URL']}` : undefined;
+
 // https://astro.build/config
 export default defineConfig({
+  site: site,
   integrations: [tailwind()],
   markdown: {
     remarkPlugins: [remarkToc],
