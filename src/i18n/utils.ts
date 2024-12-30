0a5673db143ec1b0d8be7c8ea8fc275ac20a868e
kurotu <kurotu@gmail.com>
2024-12-30 20:45:24 +0900

    Add alternate tags

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,7 +1,12 @@
-import { ui, defaultLang } from './ui';
+import { ui, defaultLang, type Language } from './ui';
 
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof typeof ui[typeof defaultLang]) {
     return ui[lang][key] || ui[defaultLang][key];
   }
+}
+
+export function getRelativePath(currentLocale: Language, relativeLocaleUrl: string) {
+  const localePath = currentLocale === defaultLang ? '/' : `/${currentLocale}/`;
+  return relativeLocaleUrl.replace(localePath, '').replace(/\/$/, '');
 }
