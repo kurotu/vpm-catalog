492d43b1ef6ce6003c4458f047df87c3dc958200
kurotu <kurotu@gmail.com>
2025-07-18 01:09:54 +0900

    Stop using YEAR_WEEK for cache key

--- tasks/generate-cache-key.sh
+++ tasks/generate-cache-key.sh
@@ -5,6 +5,5 @@ BRANCH="$VERCEL_GIT_COMMIT_REF"
 if [ "$BRANCH" = "" ]; then
   BRANCH=$(git rev-parse --abbrev-ref HEAD)
 fi
-YEAR_WEEK=$(date +%Y-week%V)
 
-echo "vpm-catalog-cache-$BRANCH-$YEAR_WEEK"
+echo "vpm-catalog-cache-$BRANCH"
